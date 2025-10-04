ccba10a1438bc35225ec521db6c24bd5d74ee13b
murphys4872 <131992625+murphys4872@users.noreply.github.com>
2025-10-04 17:08:28 -0400

    Add files via upload

---
+++ P1LAB1_MurphyShemar.py
@@ -0,0 +1,10 @@
+# Shemar Murphy
+# October 4th, 2025
+# Assignment: P1LAB1 
+# Description: This program reads input and prints output.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display a welcome message that matches the example
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
